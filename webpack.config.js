ef38f36ba573ce107cd8cb90edd5dd7107826d87
yechangxiao <yechangxiaoycx@qq.com>
2022-05-13 11:22:03 +0800

    [feat]: 添加dev-server的proxy代理

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,19 @@ module.exports = {
   },
   devServer: {
     static: true, // 开发环境下访问静态资源，代替copy-webpack-plugin，默认开启
+    proxy: {
+      '/api': {
+        // http://localhost:8080/api/users -> https://api.github.com/api/users
+        target: 'https://api.github.com',
+        // http://localhost:8080/api/users -> https://api.github.com/users
+        pathRewrite: {
+          '^/api': ''
+        },
+        // 默认情况下，代理时会保留主机头的来源localhost:8080，可以将 changeOrigin 设置为 true 以覆盖此行为。
+        // 因为服务器会有多个网站，需要请求的主机名来确定你访问的是哪个网站
+        changeOrigin: true
+      }
+    }
   },
   resolveLoader: {
     modules: ['node_modules', './'], // 添加loader的第二种路径处理
