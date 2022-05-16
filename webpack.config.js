7c1bab330e081f777bb67bd675c83b827de8a937
yechangxiao <yechangxiaoycx@qq.com>
2022-05-16 20:15:31 +0800

    [fix]: 解决图片引入出错的问题

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,17 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyRightWebpackPlugin = require('./copyright-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin') // webpack < 5的时候这个插件版本得小于7
+// const CopyWebpackPlugin = require('copy-webpack-plugin') // webpack < 5的时候这个插件版本得小于7
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
-
-
 
 module.exports = {
   mode: 'none', // webpack为每种模式预设了一些配置
   entry: './src/main.js',
   output: {
-    filename: 'bundle.[contenthash:8].js',
-    path: path.join(__dirname, 'dist'), // 必须是一个绝对路径
+    filename: 'bundle.[contenthash:8].js', // 开发模式下只能使用hash类型的名称
+    path: path.join(__dirname, 'dist') // 必须是一个绝对路径
     // publicPath: 'dist/'
   },
   optimization: {
@@ -45,12 +43,12 @@ module.exports = {
         // 因为服务器会有多个网站，需要请求的主机名来确定你访问的是哪个网站
         changeOrigin: true
       }
-    },
+    }
     // hot: false, // webpack4已经默认开启了，插件也不需要加了
     // hot: 'only', // 在热更新失败时不刷新页面进行回退，防止错误信息被刷新掉
   },
   resolveLoader: {
-    modules: ['node_modules', './'], // 添加loader的第二种路径处理
+    modules: ['node_modules', './'] // 添加loader的第二种路径处理
   },
   module: {
     rules: [
@@ -61,15 +59,17 @@ module.exports = {
             loader: MiniCssExtractPlugin.loader
           },
           // 'style-loader',
-          'css-loader', // 只是打包css，需要其他loader来引入
+          'css-loader' // 只是打包css，需要其他loader来引入
         ]
       },
       {
-        test: /.png$/,
+        test: /.(png|jp?g|gif)$/,
         use: {
           loader: 'url-loader', // 超过limit大小的会自动调用file-loader进行处理
           options: {
-            limit: 10 * 1024
+            esModule: false, // 禁止使用ESM加载图片，否则引入图片会出错
+            limit: 10 * 1024,
+            name: 'img/[name].[contenthash:6].[ext]'
           }
         }
       },
@@ -82,7 +82,7 @@ module.exports = {
             options: {
               presets: [
                 ['@babel/preset-env', {
-                  modules: false, // 默认是auto，为了保证ESM不被转换，用于tree-shaking，强制设为false
+                  modules: false // 默认是auto，为了保证ESM不被转换，用于tree-shaking，强制设为false
                 }]
               ]
             }
@@ -90,7 +90,7 @@ module.exports = {
         ]
       },
       // {
-      //   test: /.html$/, // 
+      //   test: /.html$/, //
       //   use: {
       //     // 处理html中的文件加载，注意与html-webpack-plugin区分
       //     // 这个loader会影响到html-webpack-plugin，比如使用<%= htmlWebpackPlugin.options.title %>不能进行解析
@@ -125,19 +125,19 @@ module.exports = {
     // 用于生成index.html
     new HtmlWebpackPlugin({ // 在webpack4和bable7下，必须指定template，不然就会出错
       title: 'webpack plugin sample',
-      meta: { 
+      meta: {
         viewport: 'width=device-width'
       },
       filename: 'index.html',
       template: 'public/index.html',
       minify: false, // 禁用html压缩，当html模板使用了options的时候，在压缩的时候会出错
       inject: true,
-      chunks: ['index'], // 为多入口打包时指定注入的chunk
+      chunks: ['index'] // 为多入口打包时指定注入的chunk
     }),
     // 生成about.html，用于多页面打包
     new HtmlWebpackPlugin({
       filename: 'about.html',
-      template: 'public/about.html',
+      template: 'public/about.html'
       // chunks: ['about'], // 为多入口打包时指定注入的chunk
     }),
     // 开发阶段最好不要使用这个插件，因为开发下打包频繁，拷贝文件费性能
